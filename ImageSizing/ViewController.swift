318949fb4e6d6d07db5b9be9a4ce00f98ca8f084
joel <joelcrawford30@gmail.com>
2021-07-06 10:08:38 +0300

    Image array

--- ImageSizing/ViewController.swift
+++ ImageSizing/ViewController.swift
@@ -23,12 +23,28 @@ class TableViewController: UITableViewController {
     
     
     var images =  [#imageLiteral(resourceName: "image1"),#imageLiteral(resourceName: "image2"),#imageLiteral(resourceName: "image3"),#imageLiteral(resourceName: "image4"),#imageLiteral(resourceName: "image5"),#imageLiteral(resourceName: "image6"),#imageLiteral(resourceName: "image7"),#imageLiteral(resourceName: "image8"),#imageLiteral(resourceName: "image9"),#imageLiteral(resourceName: "image10")]
+    
+    var imagesArray:[UIImage] = []
         
         //[image1, image2,image3,image4,image5,image6,image7,image8,image9,image10]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
+        
+        imagesArray.append(UIImage(named: "image1.png")!)
+        imagesArray.append(UIImage(named: "image2.png")!)
+        imagesArray.append(UIImage(named: "image3.png")!)
+        imagesArray.append(UIImage(named: "image4.png")!)
+        imagesArray.append(UIImage(named: "image5.png")!)
+        imagesArray.append(UIImage(named: "image6.png")!)
+        imagesArray.append(UIImage(named: "image7.png")!)
+        imagesArray.append(UIImage(named: "image8.png")!)
+        imagesArray.append(UIImage(named: "image9.png")!)
+        imagesArray.append(UIImage(named: "image10.png")!)
+        
         
         //======Register the table view cell
         self.tableView.register(ImageViewCell.self, forCellReuseIdentifier: "imageViewCell")
@@ -38,14 +54,14 @@ class TableViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return images.count
+        return imagesArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "imageViewCell") as! ImageViewCell
         
-        cell.mainImageView.image = images[indexPath.row]
+        cell.mainImageView.image = imagesArray[indexPath.row]
 
         return cell
         
@@ -53,7 +69,7 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        let currentImage = images[indexPath.row]
+        let currentImage = imagesArray[indexPath.row]
         let imageCropped = currentImage.getCropRation()
         return tableView.frame.width / imageCropped //if you worked for the HeightRatio,, you would multiply here
         
